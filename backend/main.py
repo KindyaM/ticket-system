c7f2429dff72a3ab1641e15dc00e7c596333c403
KindyaM <kindyamercy@gmail.com>
2026-04-03 15:25:48 +0100

    created the update status for each ticket

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+tickets = []
 
 @app.post("/tickets")
 def create_ticket():
@@ -13,3 +14,7 @@ def get_tickets():
 @app.get("/tickets/{ticket_id}")
 def get_ticket(ticket_id: int):
     return {"ticket_id": ticket_id} 
+
+@app.put("/tickets/{ticket_id}")
+def update_ticket(ticket_id: int, status:str):
+    return {"ticket_id": ticket_id, "status": status}
