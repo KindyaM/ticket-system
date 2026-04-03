ce77ca2fb88fc4c6926099e5b299e85f7526520a
KindyaM <kindyamercy@gmail.com>
2026-04-03 15:08:37 +0100

    created  a method to obtain a single ticket

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.post("/tickets")
 def create_ticket():
     return {"status": "ticket created"}
+
+@app. get("/tickets")
+def get_tickets():
+    return tickets
+
+@app.get("/tickets/{ticket_id}")
+def get_ticket(ticket_id: int):
+    return {"ticket_id": ticket_id} 
