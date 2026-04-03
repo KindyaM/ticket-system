bcb9451b6876e7aaa5dba27293fe6ceacd8b27ae
KindyaM <kindyamercy@gmail.com>
2026-04-03 15:58:48 +0100

    connected it to the database and its CRUD following

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine, Base
+from models import Ticket
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-tickets = []
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.post("/tickets")
-def create_ticket():
-    return {"status": "ticket created"}
+def create_ticket(title: str, description: str, db: Session = Depends(get_db)):
+    ticket = Ticket(title=title, description=description)
+    db.add(ticket)
+    db.commit()
+    db.refresh(ticket)
+    return ticket
 
-@app. get("/tickets")
-def get_tickets():
-    return tickets
+@app.get("/tickets")
+def get_tickets(db: Session = Depends(get_db)):
+    return db.query(Ticket).all()
 
 @app.get("/tickets/{ticket_id}")
-def get_ticket(ticket_id: int):
-    return {"ticket_id": ticket_id} 
+def get_ticket(ticket_id: int, db: Session = Depends(get_db)):
+    return db.query(Ticket).filter(Ticket.id == ticket_id).first()
 
 @app.put("/tickets/{ticket_id}")
-def update_ticket(ticket_id: int, status:str):
-    return {"ticket_id": ticket_id, "status": status}
+def update_ticket(ticket_id: int, status: str, db: Session = Depends(get_db)):
+    ticket = db.query(Ticket).filter(Ticket.id == ticket_id).first()
+    ticket.status = status
+    db.commit()
+    return ticket
+
+@app.delete("/tickets/{ticket_id}")
+def delete_ticket(ticket_id: int, db: Session = Depends(get_db)):
+    ticket = db.query(Ticket).filter(Ticket.id == ticket_id).first()
+    if not ticket:
+        return {"error": f"Ticket {ticket_id} not found"}
+    db.delete(ticket)
+    db.commit()
+    return {"message": f"Ticket {ticket_id} deleted"}
