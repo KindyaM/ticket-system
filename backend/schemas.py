0f2485bc1b8c575473eface204f09c37d80933ee
KindyaM <kindyamercy@gmail.com>
2026-08-23 16:48:49 +0100

    created a user schema

--- backend/schemas.py
+++ backend/schemas.py
@@ -4,4 +4,7 @@ class TicketCreate(BaseModel):
     title: str = Field(..., min_length = 1)
     description: str = Field(..., min_length = 1)
 
-    
+class UserCreate(BaseModel):
+    email: str = Field(..., min_length = 1)
+    password: str = Field(..., min_length = 1)
+    role: str = Field(...)    
